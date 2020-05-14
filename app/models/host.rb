0b886d7921d9cc2f38557528b0fca30d8ab02f81
Malaber <git@schaedler.rocks>
2020-05-14 19:57:06 +0200

    copy pasta weird stuff

--- app/models/host.rb
+++ app/models/host.rb
@@ -1,6 +1,6 @@
 class Host < ApplicationRecord
   belongs_to :environment, optional: true
-  has_and_belongs_to_many :hosts
+  has_and_belongs_to_many :databases
 
   validates :hostname, uniqueness: true
   validates :ip, format: { with: /\A(?:[0-9]{1,3}\.){3}[0-9]{1,3}\z/,
