5af6b89dc10099e3ebeedc8f4b1643402db89fe6
Malaber <git@schaedler.rocks>
2020-05-14 18:28:28 +0200

    rails g scaffold

---
+++ db/migrate/20200514162646_create_hosts.rb
@@ -0,0 +1,12 @@
+class CreateHosts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :hosts do |t|
+      t.string :hostname
+      t.string :ip
+      t.string :operating_system
+      t.belongs_to :environment, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
