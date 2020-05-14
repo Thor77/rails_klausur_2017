b6eae9db75da1a8be09ca7a18a0270bc3292f6cb
Malaber <git@schaedler.rocks>
2020-05-14 19:47:46 +0200

    validate all the things

--- app/models/database.rb
+++ app/models/database.rb
@@ -1,4 +1,6 @@
 class Database < ApplicationRecord
   has_and_belongs_to_many :hosts
   has_and_belongs_to_many :users
+
+  validates :name, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
   has_and_belongs_to_many :users
+
+  validates :email, presence: true
 end
